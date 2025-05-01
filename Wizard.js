0b744d747c20f3ffa7eba9264715d9a8ef983fcf
MDinosaur <onlyforboogie@icloud.com>
2025-05-01 03:36:28 -0400

    Update Wizard.js

--- Wizard.js
+++ Wizard.js
@@ -13,8 +13,8 @@ export default class Wizard extends Phaser.Scene {
 
     }
         create () {
-            this.add.image(800, 600, 'Wizard');
-            this.add.text(300,0, 'Press enter to go back to the beginning', {font: 'American Typewriter'});
+            this.add.image(500, 400, 'Wizard');
+            this.add.text(300, 80, 'Press enter to go back to the beginning', {font: 'American Typewriter'});
 
     this.input.keyboard.on('keyup-ENTER', this.handleEnterKey, this);
          }
