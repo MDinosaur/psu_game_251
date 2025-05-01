da721189afc8fe8757fcdc9c2948032e93ea42f7
MDinosaur <onlyforboogie@icloud.com>
2025-05-01 01:30:01 -0400

    Create Nostalgia.js

---
+++ Nostalgia.js
@@ -0,0 +1,35 @@
+import Calling from './Calling.js'
+export default class Nostalgia extends Phaser.Scene {
+   constructor() {
+     super({ key: 'Nostalgia' });
+   }
+
+      function preload () {
+    console.log('Nostalgia preloading assets...');
+    this.load.image('Calling', 'Transitions/Calling.png'); 
+    this.load.image('Exist', 'Transitions/Exist.png');
+    this.load.image('Nostalgia', 'Transitions/Nostalgia core.png');
+    this.load.image('Poetry', 'Transitions/Poetry.png');
+    this.load.image('Wizard', 'Transitions/Wizard.png');
+
+    }
+        create () {
+            this.add.image(800, 600, 'Nostalgia');
+            this.add.text(300,0, 'Press enter to go back to the beginning', {font: 'American Typewriter'});
+
+    const enterKey = this.input.keyboard.on('keyup-ENTER', listener);
+         enterKey.on('enter', () => {
+           console.log('Enter Key was pressed')
+             //this.add.start('Calling');
+        });
+    }
+}
+
+var config = {
+    type: Phaser.AUTO,
+    width: 1600,
+    height: 1200,
+    scene: [Calling, Nostalgia],
+};
+
+const game = new Phaser.Game(config);
